4ab69e228fd1f7953bd40c57ab42ba88b454a6a2
panjiarif <panjiarif.jr@gmail.com>
2025-09-13 10:35:30 +0700

    memindahkan dalam satu file auth

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { CLogin } from "../controllers/auth.controller.js";
+import { CCreateAdmin, CDeleteAdmin, CLogin, CUpdateAdmin } from "../controllers/auth.controller.js";
+import { VCreateAdmin, VDeleteAdmin, VUpdateAdmin } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
 router.post("/login", CLogin);
+router.post('/create', VCreateAdmin, CCreateAdmin);
+router.put('/:id', VUpdateAdmin, CUpdateAdmin);
+router.delete('/:id', VDeleteAdmin, CDeleteAdmin);
 
 export default router;
